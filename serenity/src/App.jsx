8e78667cd74bbbb867d223b14f7fc7123e54979a
ryan <concepting@protonmail.com>
2024-05-13 22:28:29 -0400

    still trying to make site responsive

--- serenity/src/App.jsx
+++ serenity/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="responsive">
         <div className='homePage'>
           <div className="logoContainer">
             <h1 className="logo emitting-text">Serenity  </h1>
@@ -32,38 +32,8 @@ function App() {
         <div className='footerPage'>
           <Footer />
         </div>    
-    </>
+    </div>
   )
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  const container = document.querySelector('.container');
-  const emittingText = document.querySelector('.emitting-text');
-
-  emittingText.addEventListener('animationiteration', () => {
-    const particle = document.createElement('div');
-    particle.classList.add('firework-particle');
-    particle.style.backgroundColor = randomColor();
-    particle.style.opacity = Math.random() * 0.6 + 0.4; // Random opacity between 0.4 and 1.0
-    particle.style.left = `${Math.random() * emittingText.offsetWidth}px`;
-    container.appendChild(particle);
-
-    setTimeout(() => {
-      particle.remove();
-    }, 2000); // Remove particle after 2 seconds
-  });
-
-  function randomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-});
-
-
-
 
 export default App
