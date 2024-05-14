c69b147a37b0fd7b2acea4db349e623c5604c972
ryan <concepting@protonmail.com>
2024-05-13 22:40:21 -0400

    omg i think i fucked something up

--- serenity/src/App.jsx
+++ serenity/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="responsive">
         <div className='homePage'>
           <div className="logoContainer">
-            <h1 className="logo emitting-text">Serenity  </h1>
+            <h1 className="logo emitting-text">Serenity</h1>
             <p className="tagline">calming the nerves.</p>
           </div>
         </div>
@@ -31,39 +31,9 @@ function App() {
             
         <div className='footerPage'>
           <Footer />
-        </div>    
-    </>
+        </div>     
+    </div>
   )
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  const container = document.querySelector('.container');
-  const emittingText = document.querySelector('.emitting-text');
-
-  emittingText.addEventListener('animationiteration', () => {
-    const particle = document.createElement('div');
-    particle.classList.add('firework-particle');
-    particle.style.backgroundColor = randomColor();
-    particle.style.opacity = Math.random() * 0.6 + 0.4; // Random opacity between 0.4 and 1.0
-    particle.style.left = `${Math.random() * emittingText.offsetWidth}px`;
-    container.appendChild(particle);
-
-    setTimeout(() => {
-      particle.remove();
-    }, 2000); // Remove particle after 2 seconds
-  });
-
-  function randomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-});
-
-
-
 
 export default App
